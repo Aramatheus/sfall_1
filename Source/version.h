ba0b5b3b8489f72f568e9c1a5567b93fc9426dfe
Sduibek <falloutfixt@gmail.com>
2015-07-04 03:31:37 -0700

    Version updated

--- Source/version.h
+++ Source/version.h
@@ -3,8 +3,8 @@
 #define TARGETVERSION "Fallout 1 v1.2 US"
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 19
+#define VERSION_MINOR 20
 #define VERSION_BUILD 0
 #define VERSION_REV 0
 
-#define VERSION_STRING "1.19"
+#define VERSION_STRING "v1.20"
